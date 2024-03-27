0767a50664903857b4bdfec52bbecc75e995c9db
lucianatux <lucianatux@yahoo.com.ar>
2024-03-27 19:48:56 -0300

    estilos

--- revista/src/components/Home.js
+++ revista/src/components/Home.js
@@ -1,51 +1,36 @@
-export const Home = ()=> {
-    return(
-        <div className="section-app">
-            <h3 className="text-light px-4 indice">Indice</h3>
-            <div className="row">
-            <div className="themes col-2 tema1"></div>
-            <div className="titles col-10">"El cuerpo es nuestra herramienta maestra"</div>
-            </div>
-            <div className="row">
-            <div className="themes col-2 tema2"></div>
-            <div className="titles col-10">"El cuerpo es nuestra herramienta maestra"</div>
-            </div>
-            <div className="row">
-            <div className="themes col-2 tema3"></div>
-            <div className="titles col-10">"El cuerpo es nuestra herramienta maestra"</div>
-            </div>
-            <div className="row">
-            <div className="themes col-2 tema1"></div>
-            <div className="titles col-10">"El cuerpo es nuestra herramienta maestra"</div>
-            </div>
-            <div className="row">
-            <div className="themes col-2 tema2"></div>
-            <div className="titles col-10">"El cuerpo es nuestra herramienta maestra"</div>
-            </div>
-            <div className="row">
-            <div className="themes col-2 tema3"></div>
-            <div className="titles col-10">"El cuerpo es nuestra herramienta maestra"</div>
-            </div>
-            {/*
-              <div className="row">
-        <div className="themes col-md-2">
-          <div className="themes tema1"></div>
-          
-          <div className="themes tema2"></div>
-          <div className="themes tema3"></div>
-          <div className="tema1"></div>
-          <div className="tema2"></div>
-          <div className="tema3"></div>
+export const Home = () => {
+  return (
+    <div className="section-app">
+      <div className="row">
+        <div className="themes col-sm-12 col-md-3">
+          <h3 className="indice">Indice</h3>
         </div>
-        <div className="titles col-md-10">
-          <div className="title1">"El cuerpo es nuestra herramienta maestra"</div>
-          <div className="title2">"Sociedades y convivencias"</div>
-          <div className="title3">"El consumismo y la caída del sueño americano"</div>
+      </div>
+      <div className="row">
+        <div className="themes col-sm-12 col-md-3">
+          <div className=" theme tema1"></div>
         </div>
+        <div className="titles col-sm-12 col-md-9">
+          "El cuerpo es nuestra herramienta maestra"
         </div>
-            
-            */ }
-      
+      </div>
+      <div className="row">
+        <div className="themes col-sm-12 col-md-3 ">
+          <div className=" theme tema2"></div>
         </div>
-    )
-}
+        <div className="titles col-sm-12 col-md-9">
+          "Sociedades y convivencias"
+        </div>
+      </div>
+      <div className="row">
+        <div className="themes col-sm-12 col-md-3 ">
+          <div className=" theme tema3"></div>
+        </div>
+        <div className="titles col-sm-12 col-md-9 ">
+          "El consumismo y la caída del sueño americano"
+        </div>
+      </div>
+
+    </div>
+  );
+};
