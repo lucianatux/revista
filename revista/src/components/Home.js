22a1b40909364114199916a962726ccfa1b29363
lucianatux <lucianatux@yahoo.com.ar>
2024-04-05 16:03:07 -0300

    estilos

--- revista/src/components/Home.js
+++ revista/src/components/Home.js
@@ -6,54 +6,57 @@ export const Home = () => {
           <h3 className="indice">Indice</h3>
         </div>
       </div>
-      <a href="#one" className="text-light">
-      <div className="row">
+      <a href="#one">
+      <div className="row menu-item">
         <div className="themes col-sm-12 col-md-3">
           <div className=" theme tema1"></div>
         </div>
         
-        <div className="titles col-sm-12 col-md-9">
+        <div className="titles col-sm-12 col-md-4">
           El cuerpo es nuestra mayor herramienta
         </div>
       </div></a>
-      <a href="#two" className="text-light">
-      <div className="row">
+      <a href="#two">
+      <div className="row menu-item">
         <div className="themes col-sm-12 col-md-3 ">
           <div className=" theme tema2"></div>
         </div>
-        <div className="titles col-sm-12 col-md-9">
+        <div className="titles col-sm-12 col-md-4">
         Explorando Nuevas Formas de Organización y Vida
         </div>
       </div>
       </a>
-      <a href="#three" className="text-light">
-      <div className="row">
+      <a href="#three" >
+      <div className="row menu-item">
         <div className="themes col-sm-12 col-md-3 ">
           <div className=" theme tema3"></div>
         </div>
-        <div className="titles col-sm-12 col-md-9 ">
+        <div className="titles col-sm-12 col-md-4 ">
         Una nueva sensibilidad está naciendo
         </div>
       </div>
       </a>
-      <a href="#game" className="text-light">
-      <div className="row">
+      <a href="#game" >
+      <div className="menu-item row">
         <div className="themes col-sm-12 col-md-3 ">
           <div className=" theme tema4"></div>
         </div>
-        <div className="titles col-sm-12 col-md-9 ">
+        <div className="titles col-sm-12 col-md-4 ">
           Juego de la Memoria
         </div>
       </div>
       </a>
-      <div className="row">
+      {/*
+          <div className="row">
         <div className="themes col-sm-12 col-md-3 ">
           <div className=" theme tema1"></div>
         </div>
-        <div className="titles col-sm-12 col-md-9 ">
+        <div className="titles col-sm-12 col-md-4 ">
           Agradecimientos
         </div>
       </div>
+      */}
+  
     </div>
   );
 };
