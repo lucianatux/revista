6a46c8c2a2e9aee51e793f26791862bce9f380a6
lucianatux <lucianatux@yahoo.com.ar>
2024-04-30 13:28:19 -0300

    home

--- revista/src/components/Home.js
+++ revista/src/components/Home.js
@@ -2,50 +2,33 @@ export const Home = () => {
   return (
     <div className="section-app" id="inicio">
       <div className="row">
-        <div className="themes col-sm-12 col-md-3">
-          <h3 className="indice">Indice</h3>
+        <div className="themes col-sm-12 col-md-12">
+          <h2 className="indice">Indice</h2>
         </div>
       </div>
-      <a href="#one">
-      <div className="row menu-item">
-        <div className="themes col-sm-12 col-md-3">
-          <div className=" theme tema1"></div>
+      <div className="row">
+        <div className="themes col-sm-12 col-md-4">
+          <a href="#one" className="menu-item">
+            <div className=" theme tema1"></div>
+          </a>
         </div>
-        
-        <div className="titles col-sm-12 col-md-4">
-          El cuerpo es nuestra mayor herramienta
+        <div className="themes col-sm-12 col-md-4 ">
+          <a href="#two" className="menu-item">
+            <div className=" theme tema2"></div>
+          </a>
         </div>
-      </div></a>
-      <a href="#two">
-      <div className="row menu-item">
-        <div className="themes col-sm-12 col-md-3 ">
-          <div className=" theme tema2"></div>
+        <div className="themes col-sm-12 col-md-4 ">
+          <a href="#three" className="menu-item">
+            <div className=" theme tema3"></div>
+          </a>
         </div>
-        <div className="titles col-sm-12 col-md-4">
-        La Forma en las Relaciones
-        </div>
-      </div>
-      </a>
-      <a href="#three" >
-      <div className="row menu-item">
-        <div className="themes col-sm-12 col-md-3 ">
-          <div className=" theme tema3"></div>
-        </div>
-        <div className="titles col-sm-12 col-md-4 ">
-        Una nueva sensibilidad está naciendo
+        <div className="themes col-sm-12 col-md-4 ">
+          <a href="#game" className="menu-item">
+            <div className=" theme tema4"></div>
+          </a>
         </div>
       </div>
-      </a>
-      <a href="#game" >
-      <div className="menu-item row">
-        <div className="themes col-sm-12 col-md-3 ">
-          <div className=" theme tema4"></div>
-        </div>
-        <div className="titles col-sm-12 col-md-4 ">
-          Juego de la Memoria
-        </div>
-      </div>
-      </a>
+
       {/*
           <div className="row">
         <div className="themes col-sm-12 col-md-3 ">
@@ -56,7 +39,6 @@ export const Home = () => {
         </div>
       </div>
       */}
-  
     </div>
   );
 };
