7f9948c264fff50027c0722d1e7d18a98f027275
lucianatux <lucianatux@yahoo.com.ar>
2024-05-11 13:59:01 -0300

    mas cambios

--- revista/src/components/Home.js
+++ revista/src/components/Home.js
@@ -1,6 +1,6 @@
 export const Home = () => {
   return (
-    <div >
+    <div className="section-app" >
       <div className="row p-5">
         <div className="themes col-sm-12 col-md-6">
           <a href="#one" className="menu-item">
