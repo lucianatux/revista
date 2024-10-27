54753e08d0f05ce0bea691e9c90c95521c993228
lucianatux <lucianatux@yahoo.com.ar>
2024-10-27 16:18:26 -0300

    update about us

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -6,20 +6,136 @@ export const Aboutus = () => {
       <h1 className="aboutus-title">Nuestro propósito</h1>
       <img className=" aboutus-img" src={group} alt="img group" />
       <p className="article-p aboutus-text">
-        Bienvenido a la Revista Digital <span>El Ojo Cuántico</span>, donde
+        Bienvenid@ a la Revista Digital <span>El Ojo Cuántico</span>, donde
         exploramos los avances y descubrimientos que están moldeando nuestra
-        comprensión del mundo y de nosotros mismos. Nos dedicamos a comunicar y
-        compartir las nuevas perspectivas que surgen en el ámbito
-        humano-espiritual, abriendo espacio para la reflexión y el crecimiento
-        personal.  Nuestra misión es compartir en este espacio todo aquello que nos
-        inspira y las nuevas formas de entender la
-        realidad. Desde análisis profundos hasta reflexiones personales, nuestra
-        revista ofrece una variedad de contenidos diseñados para estimular tu
-        mente y alimentar tu espíritu. Únete a nosotros en este viaje de
-        descubrimiento y exploración. Juntos, podemos abrirnos a nuevas
-        posibilidades y encontrar un mayor significado en nuestra existencia.
-        ¡Bienvenido a nuestra comunidad!
+        comprensión del mundo y de nosotros mismos.
+        <br />
+        <br />
+        Cuestiones que nos interesan:
+        <br />
+        <br />
+        <span className="item-title">
+          Desarrollo Humano Consciente e Integral:
+        </span>{" "}
+        A favor de un desarrollo que contemple no solo el bienestar material,
+        sino también el bienestar emocional, mental, ético, social y espiritual
+        de todos los seres humanos.
+        <br />
+        <br />
+        <span className="item-title">
+          Exploración de Potencial Humano y Energético:
+        </span>{" "}
+        Apoyo a la investigación de nuestras capacidades energéticas y
+        cognitivas menos conocidas, habilidades que podrían parecer "mágicas"
+        pero que, en más altos niveles de conciencia y despertar interior,
+        pueden ser posibles en el ser humano.
+        <br />
+        <br />
+        <span className="item-title">Conciencia:</span> A favor de prácticas que
+        fomenten la autoobservación, la conciencia de uno mismo y una conciencia
+        colectiva para mejorar las relaciones humanas.
+        <br />
+        <br />
+        <span className="item-title">
+          Reevaluación de los Valores Centrales:
+        </span>{" "}
+        En contra del dinero y el poder como valores centrales. A favor de
+        reorganizar nuestra sociedad para priorizar el aprendizaje, la
+        exploración, la coherencia y la humanidad como pilares fundamentales.
+        <br />
+        <br />
+        <span className="item-title">Educación Integral:</span>
+        A favor de una educación pública y gratuita. A favor de una educación
+        que no se limite a la acumulación de datos, sino que promueva un
+        desarrollo equilibrado y fomente valores éticos, lúdica, inteligencia
+        emocional, creatividad, pensamiento crítico, y el potencial pleno de
+        cada persona.
+        <br />
+        <br />
+        <span className="item-title">
+          Conciencia Ambiental y Sostenibilidad:
+        </span>
+        A favor de una relación consciente y responsable con el medio ambiente,
+        promoviendo el amor y el agradecimiento a nuestra madre Tierra, incentivando prácticas sostenibles y la protección de los ecosistemas.
+        <br />
+        <br />
+        <span className="item-title">Economía Consciente y Solidaria:</span> A
+        favor de un modelo económico que valore el impacto social y ambiental,
+        promoviendo la cooperación, el bienestar colectivo y una distribución
+        justa de la riqueza.
+        <br />
+        <br />
+        <span className="item-title">
+          Gobernanza Representativa y Participativa:
+        </span>{" "}
+        A favor de crear nuevas formas de gobierno que permitan al pueblo una
+        representación genuina y una participación activa en la toma de
+        decisiones.
+        <br />
+        <br />
+        <span className="item-title">
+          Cultura de la Paz y Diálogo Respetuoso:
+        </span>{" "}
+        A favor de construir una sociedad que valore la paz, el diálogo
+        constructivo y la resolución pacífica de conflictos.
+        <br />
+        <br />
+        <span className="item-title">
+          Protección de los Derechos Humanos y la Inclusión Social:{" "}
+        </span>
+        A favor de la protección universal de los derechos humanos, la igualdad
+        de género y la inclusión de todas las personas sin importar su
+        identidad, promoviendo una sociedad justa y libre de discriminación.
+        <br />
+        <br />
+        <span className="item-title">Fortalecimiento de Comunidades:</span>
+        A favor de políticas efectivas para fortalecer las comunidades locales,
+        promoviendo la economía solidaria y el desarrollo comunitario.
+        <br />
+        <br />
+        <span className="item-title">
+          Libertad de Expresión y Pluralidad de Ideas:{" "}
+        </span>
+        A favor de la libertad de expresión responsable y el diálogo respetuoso
+        entre diversas perspectivas, fomentando una sociedad abierta y
+        tolerante.
+        <br />
+        <br />
+        <span className="item-title">
+          Protección de los Derechos de los Animales:
+        </span>{" "}
+        A favor del bienestar y la protección de los animales, impulsando
+        prácticas éticas en su trato.
+        <br />
+        <br />
+        <span className="item-title">Innovación y Ciencia Responsable:</span> A
+        favor del desarrollo de la ciencia y la tecnología al servicio de la
+        sociedad y el medio ambiente, priorizando el bienestar humano.
+        <br />
+        <br />
+        <span className="item-title">Acceso Justo a la Tecnología:</span> A
+        favor de la democratización de la tecnología y un acceso igualitario.
+        <br />
+        <br />
+        <span className="item-title">
+          Defensa de la Cultura y las Humanidades:
+        </span>{" "}
+        A favor de preservar y promover la cultura y la educación humanística,
+        fomentando la creatividad y el pensamiento crítico en la sociedad.
+        <br />
+        <br />
+        <br />
+        <br />
+        Por un Futuro Compartido: Mediante nuestra revista te invitamos a
+        reflexionar, a aprender y a actuar en conjunto por un mundo mejor.
       </p>{" "}
+      <br />
+      <br />
+      <br />
+      <p>
+        Para sumarte a la creación de contenido de la revista digital "El ojo
+        cuántico" escríbemos al e-mail: elojocuantico@yahoo.com{" "}
+      </p>
     </div>
   );
 };
